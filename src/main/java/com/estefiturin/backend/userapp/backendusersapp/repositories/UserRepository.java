8b13a3fbeac5d3dadf4f34081c58e4a601652153
Estefanía Turín <tur.senn@gmail.com>
2023-07-08 13:43:18 -0300

    interface UserRepository - CRUD

---
+++ src/main/java/com/estefiturin/backend/userapp/backendusersapp/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.estefiturin.backend.userapp.backendusersapp.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository {
+
+    
+    
+}
