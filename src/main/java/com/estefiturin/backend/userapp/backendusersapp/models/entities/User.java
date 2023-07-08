e15f228f211716d27f6644b8b831a6cf367669fa
Estefanía Turín <tur.senn@gmail.com>
2023-07-08 13:31:52 -0300

    cargo proyecto...

---
+++ src/main/java/com/estefiturin/backend/userapp/backendusersapp/models/entities/User.java
@@ -0,0 +1,5 @@
+package com.estefiturin.backend.userapp.backendusersapp.models.entities;
+
+public class User {
+    
+}
---
+++ src/test/java/com/estefiturin/backend/userapp/backendusersapp/BackendUsersappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estefiturin.backend.userapp.backendusersapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendUsersappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
